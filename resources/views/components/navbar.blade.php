10ae0908b1bd3c9868e8c6ffe1c990ce7d25ce04
lpdadpl <albertodepablolopez@gmail.com>
2025-05-17 18:42:51 +0200

    feat: agregar botón de menú responsivo y mejorar iconos en la navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -58,6 +58,17 @@
         /* Para evitar que el contenido se oculte debajo de la navbar */
     }
 
+    .menu-toggle {
+        display: none; /* Oculto por defecto en pantallas grandes */
+        align-items: center;
+        justify-content: center;
+        background-color: #0056b3;
+        color: white;
+        border: none;
+        font-size: 1.5rem;
+        cursor: pointer;
+    }
+
     /* Estilos responsivos */
     @media (max-width: 768px) {
         .navbar {
@@ -65,6 +76,10 @@
             flex-direction: column;
             align-items: flex-start;
             padding: 1rem;
+        }
+
+        .menu-toggle {
+            display: flex; /* Mostrar el botón solo en pantallas pequeñas */
         }
 
         .navbar-menu {
@@ -89,17 +104,6 @@
         .navbar-end {
             margin-top: 1rem;
         }
-
-        .menu-toggle {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            background-color: #0056b3;
-            color: white;
-            border: none;
-            font-size: 1.5rem;
-            cursor: pointer;
-        }
     }
 </style>
 
@@ -112,10 +116,30 @@
     </div>
 
     <div class="navbar-menu">
-        <a href="/documents" class="nav-item">Documentos</a>
-        <a href="/books" class="nav-item">Libros</a>
-        <a href="/videos" class="nav-item">Videos</a>
-        <a href="/library" class="nav-item">Guardados</a>
+        <a href="/documents" class="nav-item">
+            Documentos
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" style="display: inline-block; margin-left: 5px; vertical-align: middle; width: 16px; height: 16px;">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 13.5H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+            </svg>
+        </a>
+        <a href="/books" class="nav-item">
+            Libros
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" style="display: inline-block; margin-left: 5px; vertical-align: middle; width: 16px; height: 16px;">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+            </svg>
+        </a>
+        <a href="/videos" class="nav-item">
+            Videos
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" style="display: inline-block; margin-left: 5px; vertical-align: middle; width: 16px; height: 16px;">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
+            </svg>
+        </a>
+        <a href="/library" class="nav-item">
+            Guardados
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" style="display: inline-block; margin-left: 5px; vertical-align: middle; width: 16px; height: 16px;">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m3 3 1.664 1.664M21 21l-1.5-1.5m-5.485-1.242L12 17.25 4.5 21V8.742m.164-4.078a2.15 2.15 0 0 1 1.743-1.342 48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185V19.5M4.664 4.664 19.5 19.5" />
+            </svg>
+        </a>
     </div>
 
     <div class="navbar-end">
